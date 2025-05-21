e0c3f5b232bedba7c968478f5d41eea2d8f9dedf
NandhanaRajendran <nandhanarajendran2004@gmail.com>
2025-05-21 15:36:58 +0530

    Solved finding median

---
+++ 21-05-25/medianOfMergedArrays.js
@@ -0,0 +1,52 @@
+/* LEVEL : Hard
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+ 
+
+Constraints:
+
+    nums1.length == m
+    nums2.length == n
+    0 <= m <= 1000
+    0 <= n <= 1000
+    1 <= m + n <= 2000
+    -106 <= nums1[i], nums2[i] <= 106
+
+*/
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+
+    let merged = nums1.concat(nums2);
+    merged.sort((a,b)=>a-b);
+    let median; 
+    let len = merged.length;
+    if((len%2) == 0) {
+        median = (merged[ len/2 ] + merged[ (len/2) -1 ])/2;
+    } else {
+        median = merged[ Math.floor(len/2) ];
+    }
+    return median;
+
+
+};
